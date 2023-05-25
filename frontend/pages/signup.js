fd74e7dcc6c85283a173f10d2f1b3d43fa6e72b0
Henry Marken <77091181+HenryMarken@users.noreply.github.com>
2023-05-25 15:31:10 -0600

    frontend connection to signup api

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,20 +1,52 @@
 //third party imports
 import React, { useState } from "react";
+import { useRouter }  from 'next/router';
 
 //local imports
 import NavBar from "../components/NavBar";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
+  
+
+  const signupSubmitHandler = async event => {
+    event.preventDefault();
+
+    const formData = {
+      email,
+      name,
+      password
+    };
+    
+
+    await fetch('http://localhost:5000/api/users/signup', {
+      method:'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log('Error:', err)
+    })
+
+    router.push('/')
+     
+  }
 
   return (
     <React.Fragment>
       <NavBar />
       <div className="h-screen bg-white text-black font-playfair text-center flex flex-col items-center py-10 space-y-4">
         <p className="block flex">Join the Betweeen the Line!</p>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={signupSubmitHandler}>
         <div>
             <label className="px-5">Email:</label>
             <input
@@ -28,8 +60,8 @@ const Signup = () => {
             <label className="px-5">Username:</label>
             <input
               type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="border active:bg-gray-400"
             />
           </div>
